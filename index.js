fb6ed01d2da6b895c24c715537c56f5bbc13f0ac
Jonathan Diaz <jonathanpdiaz@gmail.com>
2020-04-21 15:29:31 -0300

    Update info endpoint

--- index.js
+++ index.js
@@ -9,15 +9,10 @@
 
 const got = require("got");
 const bitbar = require("bitbar");
-const cheerio = require("cheerio");
 const { nth, template, get, values } = require("lodash");
-const he = require("he");
 
 const STATS_INFO_URL =
   "https://www.cronista.com/MercadosOnline/json/eccheader.json?_rnd=282633634327";
-const MAE_ROFEX_URL = "http://www.mae.com.ar/mercados/Forex/Default.aspx";
-const RIESGO_URL =
-  "https://static.coins.infobae.com/cotizacion-simple/dolar-riesgo.json";
 
 const STATS_INFO_INDEX = [
   {
@@ -25,22 +20,29 @@ const STATS_INFO_INDEX = [
     replace: template(
       "🏦 $${parseFloat(valorcompra).toFixed(2)}/$${parseFloat(valorventa).toFixed(2)} ~ ${parseFloat(variacion).toFixed(2)}%"
     ),
-    index: 0
+    index: 0,
   },
   {
-    currency: "Dólar Blue",
+    currency: "Dólar Contado con Liqui",
     replace: template(
-      "🔵 $${parseFloat(valorcompra).toFixed(2)}/$${parseFloat(valorventa).toFixed(2)} ~ ${parseFloat(variacion).toFixed(2)}%"
+      "💧 $${parseFloat(valorcompra).toFixed(2)}/$${parseFloat(valorventa).toFixed(2)} ~ ${parseFloat(variacion).toFixed(2)}%"
     ),
-    index: 1
+    index: 1,
   },
   {
     currency: "Merval",
     replace: template(
-      "${parseFloat(variacion) > 0 ? '📈': '📉'} $${parseFloat(valor).toFixed(2)} ~ ${parseFloat(variacion).toFixed(2)}% ${parseFloat(variacion) < -2 ? '🔥':''}"
+      "${parseFloat(variacion) > 0 ? '📈': '📉'} $${parseFloat(valor).toFixed(2)} ~ ${parseFloat(variacion).toFixed(2)}"
     ),
-    index: 2
-  }
+    index: 2,
+  },
+  {
+    currency: "Riesgo País",
+    replace: template(
+      "${parseFloat(variacion) > 0 ? '🔥': '❄️'} ${parseFloat(valor).toFixed(2)} ~ ${parseFloat(variacion).toFixed(2)}% ${parseFloat(variacion) < -2 ? '🔥':''}"
+    ),
+    index: 3,
+  },
 ];
 
 function getMainMessage(currency) {
@@ -58,16 +60,8 @@ function getMainMessage(currency) {
   }
 }
 
-async function getMAEForex() {
-  const site = await got(MAE_ROFEX_URL);
-  const $ = cheerio.load(site.body);
-  const text = $("#ctl00_ContentPlaceHolder1_UpdatePanel1 div").html();
-  return text;
-}
-
 async function getDolarStats() {
   const info = await got(STATS_INFO_URL, { json: true });
-  const riesgo = await got(RIESGO_URL, { json: true });
   const items = values(info.body);
   const dolar = nth(items, 1);
   const message = getMainMessage(dolar);
@@ -75,7 +69,7 @@ async function getDolarStats() {
   menu.push({
     text: message,
     color: bitbar.darkMode ? "white" : "black",
-    dropdown: false
+    dropdown: false,
   });
   menu.push(bitbar.separator);
   menu = menu.concat(
@@ -83,27 +77,14 @@ async function getDolarStats() {
       const item = nth(items, index);
       return {
         text: replace(item),
-        size: 12
+        size: 12,
       };
     })
   );
   menu.push(bitbar.separator);
   menu.push({
-    text: `💣 ${get(riesgo.body, "items[1].unico")}`,
-    size: 13
-  });
-  menu.push(bitbar.separator);
-  // const mae = await getMAEForex();
-  // menu.push({
-  //   text: he.decode(mae),
-  //   href: MAE_ROFEX_URL,
-  //   length: 40,
-  //   size: 10
-  // });
-  menu.push(bitbar.separator);
-  menu.push({
     text: "🔄",
-    refresh: true
+    refresh: true,
   });
   bitbar(menu);
 }
